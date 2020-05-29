fe4338086b8e1dcfeadf2aed3420c87f4f1fd819
Skyssar <ycure10@gmail.com>
2020-05-28 21:14:45 -0500

    xd

---
+++ PenView/src/app/DatosPen/pension.ts
@@ -0,0 +1,11 @@
+export interface Pension {
+
+    codigo_pension?: number;
+    nombre_pension: string; 
+    direccion_pension: string; 
+    disponibilidad_pension: number; 
+    precio_pension: number;
+    descripcion_pension: string; 
+    id_arrendador?: number; 
+
+}
